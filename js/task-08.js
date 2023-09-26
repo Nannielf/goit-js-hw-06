e1c9ab55f28deb15e1443030c5e06144011fcc4c
Nannielf <nannielf.no@gmail.com>
2023-09-27 02:45:47 +0300

    8-1

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
 
+const forma = document.querySelector('.login-form');
+forma.addEventListener('submit', takeSubmit);
+
+function takeSubmit(evt) {
+       evt.preventDefault();
+
+    const { email, password } = evt.currentTarget.elements;
+
+    const request = {
+        email: email.value ,
+        password: password.value
+    };
+    console.log(request);
+    evt.currentTarget.reset();
+}
