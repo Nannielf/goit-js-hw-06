0164f069886a06b1c32216321ce273788c80d1b4
Nannielf <nannielf.no@gmail.com>
2023-10-02 18:01:48 +0300

    7-1

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const scrollBar = document.querySelector("#font-size-control");
 const ourText = document.querySelector("#text");
+ourText.style.fontSize = scrollBar.value + 'px';
 scrollBar.addEventListener("input", handleInputRange);
 function handleInputRange(event) {
   ourText.style.fontSize = event.currentTarget.value + 'px';
