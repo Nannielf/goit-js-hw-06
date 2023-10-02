3ac8729b373f537199041ac45b4d86f1bf84fa57
Nannielf <nannielf.no@gmail.com>
2023-10-02 18:04:20 +0300

    6-1

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const enter = document.querySelector('#validation-input');
 
 enter.addEventListener('blur', checkNumber);
 function checkNumber() {
-    if (enter.getAttribute('data-length') > enter.value.length) { 
+    if (enter.getAttribute('data-length') != enter.value.length) { 
     enter.classList.remove('valid');
     enter.classList.add('invalid');
   } else {
