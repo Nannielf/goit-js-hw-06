cd7ee7abcb5eceb18a6b423a84c9145d5911ed2b
Nannielf <nannielf.no@gmail.com>
2023-10-02 19:02:56 +0300

    1-2

--- js/task-01.js
+++ js/task-01.js
@@ -2,5 +2,5 @@ const arrItem = Array.from(document.querySelectorAll('.item'));
 console.log('Number of categories: ' + arrItem.length);
 arrItem.forEach(item => {
     console.log('Category: ' + item.firstElementChild.textContent);
-    console.log('Elements: ' + Array.from(item.querySelectorAll('li')).length);
+    console.log('Elements: ' + item.lastElementChild.children.length);
     });
